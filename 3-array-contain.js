41c4ec9f8034fbaa25477a096caa2c6fdf4f1820
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2022-04-01 16:51:45 +0300

    Update 3-array-contain.js

--- 3-array-contain.js
+++ 3-array-contain.js
@@ -1,10 +1,22 @@
-function arrayContain(array, number) {
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) { // 5
     if (array[i] === number) {
+    if (array[i] === number) { //4 
       return true;
+      return true; // 1 
+    }
     }
   }
+  }
   return false;
+  return false; // 1
+}
 }
 
-module.exports = { arrayContain };
+
+/*
+soit n = array.lenght
+n * (5 + 4 + 1) + 1
+O(n) * O(1) + O(1) 
+comme O(1) << O(n) la compléxité est O(n) 
+*/
+
