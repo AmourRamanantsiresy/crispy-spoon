a5d2d97020cbdab0fa5577edac2a2486dafb6ff6
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2022-04-01 16:39:48 +0300

    Update 1-slice.js

--- 1-slice.js
+++ 1-slice.js
@@ -1,9 +1,20 @@
 function slice(array, start, end) {
-  let slicedArray = [];
-  for (let i = start; i < end; i++) {
-    slicedArray.push(array[i]);
+  let slicedArray = []; // 1
+  for (let i = start; i < end; i++) { // 1 + 1 + 1 + 1 + 1 + 1 = 6 
+    slicedArray.push(array[i]);// 1 + 1 + 1 + 1 = 4 
   }
-  return slicedArray;
+  return slicedArray; // 1
 }
 
 module.exports = { slice };
+
+
+/*
+
+la boucle tourne end - start fois, soit n = end - start
+on an => n * (6+4) + 1 
+      => n * (10) + 1
+      => O(n) * (O(1) + (O(1))
+      comme O(1) << O(n), la compléxité de cette algorithme et O(n)
+
+*/
